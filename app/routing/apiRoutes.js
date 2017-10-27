c48a1a0becb42b1433bd622cbfbe3ee179378eab
itkazmi814 <itkazmi814@gmail.com>
2017-10-27 06:39:05 -0500

    Comparison now done using deep clone of friendsArr

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,49 +10,44 @@ function apiRoutesListeners(app) {
 	//When a new friend is posted to /api/friends/new,
 	app.post("/api/friends/new", (req, res) => { 
 		var newFriend = req.body;
-		//create a copy of the friendsArr to work off of that will not update the friendsArr reference
-		const differences = friendsArr.slice();
-		console.log(friendsArr)
-	
-		// For every existing person 
-		for(var i=0; i<differences.length; i++) {
-
-			var total = 0;
-
-			for(var j=0; j<newFriend.scores.length; j++) {
-				//compare their score against yours and add the numbers up
-				total += updateScoreTotal(newFriend.scores[j],differences[i].scores[j])
-			}
-			//Add the calculated total as a key to person we ae calculating against
-			differences[i].total = total;
-		}
-		//Sort smallest to largest by total
+		//Creates a deep copy of the friendsArr
+		var differences = JSON.parse(JSON.stringify(friendsArr))
+		//Calculates the difference in scores between newFriend and all existing people
+		differences = calculateDifferences(differences, newFriend)
+		//Sort people smallest to largest, using total
 		differences.sort((a,b) => a.total-b.total );
+		friendsArr.push(newFriend)	
+		
 		console.log("DIFFERENCES")
 		console.log(differences)
 		console.log("FRIENDS ARR")
 		console.log(friendsArr)
+		console.log("Your match: ")
+		console.log(differences[0])
 
-		var match = differences[0];
-		console.log("your match is " + match.name)
-		friendsArr.push(newFriend)	
-		
-		//Return differences[0] as the match
-		res.json(match);
+		//Return person with smallest total as the match
+		res.json(differences[0]);
 	})
 }
-		// 	//a better way to do this:
-		// 	//store the differences value in the person's object in the friendsArr
-		// 	//do differences = friendsArr and then do all of this with that array so any changes and resorting are local
-		// 	//CONCERN: updating a value in an object in differences may update in friendsArr. this happens for sure for objects, need to see if it is the case for arrays. see the code I got from daniel. 
-		// 	//pass by reference (or pointer in some languages. tells the spot in memory to point to, not the value to go to) vs pass by value 
-		// 	//look at ways to duplicate my array. try slice? then i can do a proper sort
+
+function calculateDifferences (differences,newFriend) {
+	// For every existing person 
+	for(var i=0; i<differences.length; i++) {
+		var total = 0;
+		for(var j=0; j<newFriend.scores.length; j++) {
+			//compare their score against yours and add the numbers up
+			total += updateScoreTotal(newFriend.scores[j],differences[i].scores[j])
+		}
+		//Store the calculated total as a key to person we ae calculating against
+		differences[i].total = total;
+	}
+	return differences;
+}
 
 function updateScoreTotal (newFriendScore, otherFriendScore) {
 	var newFriendInt = parseInt(newFriendScore);
 	var otherFriendInt = parseInt(otherFriendScore);
 	var diff = Math.abs(newFriendInt - otherFriendInt);
-
 	return diff;
 }
 	
